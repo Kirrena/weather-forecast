0f66f4412d6d0576f1994ccc55f450af38251265
Renata Kiraly <kirrena@gmail.com>
2023-11-20 18:14:20 +0000

    added localStorage base

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//display buttons from existing localStorage cities
+$(document).ready(function() {
+
+  createbuttonforexistinglocalstoragecities();
+  //create clear button
+
+});
+
 //select the actual day display area
 var todayEl = $("#today");
 //select the 5day forecast display area
@@ -16,16 +24,25 @@ event.preventDefault();
 //getting data from input field
 var inputCity = $("#search-input").val();
 //check City
-//console.log(inputCity);
+//calling getcoordinates function
+getcoordinates(inputCity);
+//clearing the input field
+$("#search-input").val("");
+//clear today display area
+todayEl.empty();
 
+});
 
+function getcoordinates(cityname){
 //create url for getting the coordinates of a city
 var geoUrl = "https://api.opencagedata.com/geocode/v1/json?";
-      var city = "q=" + encodeURIComponent(inputCity);
+      //encoded to ready to use as URL
+      var city = "q=" + encodeURIComponent(cityname);
       var geoKey = "&key=" + "20c415a2405347b593680460e03ca811";
       var queryUrl1 =geoUrl+city+geoKey;
       
-// run Fetch call to the OpenCageData API
+
+      // run Fetch call to the OpenCageData API
 fetch(queryUrl1)
   .then(function (response) {
     // Calling .json() to access the json data stored inside the returned promise
@@ -36,11 +53,8 @@ fetch(queryUrl1)
     getweather (data.results[0].geometry);
   });
 
-//clearing the input field
-$("#search-input").val("");
-//clear today display area
-todayEl.empty();
-});
+
+};
 
 
 
@@ -88,13 +102,67 @@ windDiv.text("Wind: "+ weatherdata.list[0].wind.speed + "KPH");
 var humDiv = $("<div/>");
 humDiv.text("Humidity: "+ weatherdata.list[0].main.humidity + "%");
 todayEl.append(cityDiv, icon, tempDiv, windDiv, humDiv);
-//create an object to store data in localStorage
-var stored = {city: weatherdata.city.name, icon: "http://openweathermap.org/img/wn/" + weatherdata.list[0].weather[0].icon + ".png",
- temperature:  "Temp: " + tempC.toFixed(2) + "Celsius", wind: "Wind: "+ weatherdata.list[0].wind.speed + "KPH", humidity: "Humidity: "+ weatherdata.list[0].main.humidity + "%" };
+//store city in a variable
+var stored = weatherdata.city.name;
+//check if the city is in the localStorage array
+//if (!JSON.parse(localStorage.getItem("cities")).includes(stored)){
+  //store city in a variable
 addtohistory(stored);
+
+//}
 }
 
-function addtohistory(object){
-  console.log(object);
+//function to creating history buttons
+function addtohistory(data){
+  var existingCities = JSON.parse(localStorage.getItem("cities")) || [];
+  if (!existingCities.includes(data)) {
+  // Add the new city to the array 
+    existingCities.push(data);
+    // Save the updated array back to localStorage
+    localStorage.setItem("cities", JSON.stringify(existingCities));
   
-}
+
+  //select history div to display buttons
+  
+  var historyButton = $("<button/>");
+  historyButton.attr("class", "history-btn");
+  historyButton.text(data);
+  //add to localStorage
+  // Retrieve the existing cities from localStorage
+  
+    historyEl.append(historyButton);
+  }
+    console.log(existingCities);
+  }
+    
+  
+
+// clicking on history button
+$("#history").on("click", ".history-btn", function(){
+  var clickedCity = $(this).text();
+
+  // Calling getcoordinates function
+  getcoordinates(clickedCity);
+    });
+
+function createbuttonforexistinglocalstoragecities(){
+    if(JSON.parse(localStorage.getItem("cities"))){
+      for (var i =0 ; i<(JSON.parse(localStorage.getItem("cities"))).length ; i++){
+      var historyButton = $("<button/>");
+      historyButton.attr("class", "history-btn");
+      historyButton.text(JSON.parse(localStorage.getItem("cities"))[i]);
+      historyEl.append(historyButton);
+    }
+  }
+}   
+
+var clearButton = $("<button/>");
+clearButton.text("Delete history");
+clearButton.attr("class", "clear btn btn-primary btn-search");
+$("#history").append(clearButton);
+
+$("#history").on("click", ".clear", function(){
+  historyEl.empty();
+  // Remove the "cities" key from localStorage
+  localStorage.removeItem("cities");
+});
